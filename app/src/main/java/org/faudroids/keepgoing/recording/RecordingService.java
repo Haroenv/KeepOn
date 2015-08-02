8d7e4a5b64621baaec4aef08a98303861f09890c
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-08-02 09:54:52 +0200

    ui: add img to recording notification

--- app/src/main/java/org/faudroids/keepgoing/recording/RecordingService.java
+++ app/src/main/java/org/faudroids/keepgoing/recording/RecordingService.java
@@ -3,9 +3,11 @@ package org.faudroids.keepgoing.recording;
 
 import android.app.Notification;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 
+import org.faudroids.keepgoing.R;
 import org.faudroids.keepgoing.utils.AbstractGoogleClientApiService;
 
 import javax.inject.Inject;
@@ -35,10 +37,14 @@ public class RecordingService extends AbstractGoogleClientApiService {
 		Timber.d("starting recording service");
 
 		// show notification while recording
-		startForeground(NOTIFICATION_ID, new Notification.Builder(this)
-				.setContentTitle("Recording")
-				.setContentText("KeepGoing is currently recording your location")
-				.build());
+		Notification.Builder notificationBuilder = new Notification.Builder(this)
+				.setContentTitle(getString(R.string.app_name))
+				.setContentText(getString(R.string.recording_new_activity))
+				.setSmallIcon(R.drawable.ic_notification);
+		if (Build.VERSION.SDK_INT >= 21) {
+			notificationBuilder.setColor(getResources().getColor(R.color.colorPrimary));
+		}
+		startForeground(NOTIFICATION_ID, notificationBuilder.build());
 
 		// keep CPU running during recording
 		wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "RecordingWakeLock");
